06e28ff627eae8101fa5ed59356a7e3cbc9b0342
RandomLorem <103768218+H4r5h-10@users.noreply.github.com>
2023-06-28 17:32:11 +0530

    "fixed the bug in contact form"

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './contact.css'
 import {HiOutlineMailOpen} from 'react-icons/hi'
 import {ImLinkedin} from 'react-icons/im'
@@ -7,12 +7,19 @@ import emailjs from '@emailjs/browser';
 import {motion} from 'framer-motion'
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_n7vxb0o', 'template_qp2rend', form.current, 'okYMoTI1x1lJsOnus')
+    emailjs.sendForm('service_n7vxb0o', 'template_qp2rend', form.current, 'okYMoTI1x1lJsOnus');
+    setEmail("");
+    setName("");
+    setMessage("");
   };
 
 
@@ -41,13 +48,20 @@ const Contact = () => {
             <a href="https://www.linkedin.com/in/harsh-agarwal-a2249313b" target="_blank" rel="noreferrer">Let's Connect</a>
           </article>
         </div>
+
+
         <form ref= {form} onSubmit={sendEmail} id="formid">
-          <input type="text" name= "name" placeholder="Your Name" onFocus={(e) => e.target.placeholder = ""} 
-  onBlur={(e) => e.target.placeholder = "Your Name"}/>
-          <input type="text" name='email' placeholder="Your Email" onFocus={(e) => e.target.placeholder = ""} 
-  onBlur={(e) => e.target.placeholder = "Your Email"}/>
-          <textarea name="message" placeholder="Leave your message here..." onFocus={(e) => e.target.placeholder = ""} 
-  onBlur={(e) => e.target.placeholder = "Leave your message here..."}></textarea>
+          <input type="text" name= "name" placeholder="Your Name" onChange={event => setName(event.target.value)} value={name}/>
+          {/* <input type="text" name= "name" placeholder="Your Name" onFocus={(e) => e.target.placeholder = ""} 
+  onBlur={(e) => e.target.placeholder = "Your Name"}/> */}
+
+
+          <input type="text" name='email' placeholder="Your Email" onChange={event => setEmail(event.target.value)} value={email}/>
+
+
+          <textarea name="message" placeholder="Leave your message here..." onChange={event => setMessage(event.target.value)} value={message}></textarea>
+
+  
           <div>
           <button className='btn btn-primary' type='submit' value='send' onClick={form.reset}><h3>Submit</h3></button>
           <button className='btn btn-primary' type='reset'><h3>Reset</h3></button>
