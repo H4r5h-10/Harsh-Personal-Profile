aa00224a6c50078781d21efa70f4cb51627c7f15
Har5h <103768218+H4r5h-10@users.noreply.github.com>
2024-04-04 18:41:13 +0530

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Cards from './Cards'
 import './projects.css'
 import image1 from '../../assets/Screenshot 2023-05-28 033426.png'
-import image2 from '../../assets/Screenshot 2023-05-29 215500.png'
+import image2 from '../../assets/Screenshot 2024-04-04 180211.png'
+import image3 from '../../assets/Screenshot 2024-04-04 183635.png'
 import {motion} from 'framer-motion'
 const Projects = () => {
   return (
@@ -18,7 +19,7 @@ const Projects = () => {
         <Cards url={image1} desc="A website to add your favourite movies to your watchlist." github="https://github.com/H4r5h-10/moviemeter" link="https://h4r5h-10.github.io/moviemeter/"/>
 {/*         <Cards url={image2} desc="A CRUD app to increase your productivity by adding tasks to your list." github="https://github.com/H4r5h-10/moviemeter" link="https://H4r5h-10.github.io/todo-list"/> */}
         <Cards url={image2} desc="A Wordle adaptation where you can play with your friends." github="https://github.com/H4r5h-10/wordle-multiplayer" link="https://wordle-multiplayer.netlify.app"/>
-        <Cards url={image2} desc="Website for SIAM-DTU" github="https://github.com/H4r5h-10/moviemeter" link="https://siamdtu.vercel.app/"/>
+        <Cards url={image3} desc="Website for SIAM-DTU" github="https://github.com/H4r5h-10/siamdtu" link="https://siamdtu.vercel.app/"/>
 
       </div>
     </motion.section>
